af1ae39a935289e09d7791f351ace74e018a097f
JohanRiv <johancamiloandres@hotmail.com>
2021-05-17 20:57:51 -0500

    Add card image description

---
+++ lib/profile_page/widgets/image_description_widget.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class ImageDescriptionWidget extends StatelessWidget {
+  String siteName = "";
+  String siteDescription = "";
+  String siteSteps = "";
+
+  ImageDescriptionWidget(this.siteName, this.siteDescription, this.siteSteps);
+
+  @override
+  Widget build(BuildContext context) {
+    Container siteNameText = Container(
+      width: MediaQuery.of(context).size.width * 0.4,
+      margin: EdgeInsets.only(top: 10, bottom: 10),
+      child: Text(
+        siteName,
+        style: TextStyle(
+            color: Colors.black,
+            fontFamily: "Lato",
+            fontSize: 17,
+            fontWeight: FontWeight.w900),
+      ),
+    );
+
+    Container siteDescriptionText = Container(
+      width: MediaQuery.of(context).size.width * 0.4,
+      margin: EdgeInsets.only(top: 5, bottom: 5),
+      child: Text(
+        siteName,
+        style: TextStyle(
+          color: Colors.black38,
+          fontFamily: "Lato",
+          fontSize: 12,
+        ),
+      ),
+    );
+
+    Container siteStepsText = Container(
+      width: MediaQuery.of(context).size.width * 0.4,
+      margin: EdgeInsets.only(top: 10, bottom: 10),
+      child: Text(
+        siteName,
+        style: TextStyle(
+            color: Colors.yellowAccent.shade400,
+            fontFamily: "Lato",
+            fontSize: 15,
+            fontWeight: FontWeight.w700),
+      ),
+    );
+
+    FloatingActionButton favoriteButton = FloatingActionButton(
+      onPressed: () {},
+      child: const Icon(
+        Icons.favorite,
+        color: Colors.white,
+      ),
+      backgroundColor: Colors.green,
+    );
+
+    Column description = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [siteNameText, siteDescriptionText, siteStepsText],
+    );
+
+    Container cardDescription = Container(
+      color: Colors.white,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          shape: BoxShape.rectangle,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black38, blurRadius: 20, offset: Offset(0, 7))
+          ]),
+      child: description,
+    );
+
+    Stack stackCardDescription = Stack(
+      alignment: Alignment(0.9, 1.1),
+      children: [cardDescription, favoriteButton],
+    );
+
+    return stackCardDescription;
+  }
+}
