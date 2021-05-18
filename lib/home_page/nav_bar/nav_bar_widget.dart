f6fbfbac385479c4fcb9b6319f34de8ae1fdd1e9
JohanRiv <johancamiloandres@hotmail.com>
2021-05-17 19:34:11 -0500

    Creación de estructura base

---
+++ lib/home_page/nav_bar/nav_bar_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavBarWidget extends StatefulWidget {
+  @override
+  _NavBarWidgetState createState() => _NavBarWidgetState();
+}
+
+class _NavBarWidgetState extends State<NavBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
