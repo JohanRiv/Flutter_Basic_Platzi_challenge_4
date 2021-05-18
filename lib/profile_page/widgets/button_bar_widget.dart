7272713e805432df593ca4a284a3391faf6978cc
JohanRiv <johancamiloandres@hotmail.com>
2021-05-17 20:30:24 -0500

    Add button row widget

---
+++ lib/profile_page/widgets/button_bar_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class ButtonBarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Container tagButton = Container(
+        margin: EdgeInsets.only(right: 20),
+        child: IconButton(
+          icon: const Icon(
+            Icons.bookmark_outline,
+            color: Colors.transparent,
+          ),
+          color: Colors.white70,
+          onPressed: () {},
+        ));
+
+    Container cardButton = Container(
+        margin: EdgeInsets.only(right: 20),
+        child: IconButton(
+          icon: const Icon(
+            Icons.card_giftcard,
+            color: Colors.transparent,
+          ),
+          color: Colors.white38,
+          onPressed: () {},
+        ));
+
+    Container addButton = Container(
+        margin: EdgeInsets.only(right: 20),
+        child: IconButton(
+          icon: const Icon(
+            Icons.bookmark_outline,
+            color: Colors.transparent,
+          ),
+          color: Colors.white,
+          iconSize: 34,
+          onPressed: () {},
+        ));
+
+    Container messageButton = Container(
+        margin: EdgeInsets.only(right: 20),
+        child: IconButton(
+          icon: const Icon(
+            Icons.mail_outline,
+            color: Colors.transparent,
+          ),
+          color: Colors.white38,
+          onPressed: () {},
+        ));
+
+    Container personButton = Container(
+        margin: EdgeInsets.only(right: 20),
+        child: IconButton(
+          icon: const Icon(
+            Icons.person,
+            color: Colors.transparent,
+          ),
+          color: Colors.white38,
+          onPressed: () {},
+        ));
+
+    Container buttonBar = Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: Row(
+        children: [
+          tagButton,
+          cardButton,
+          addButton,
+          messageButton,
+          personButton,
+        ],
+      ),
+    );
+
+    return buttonBar;
+  }
+}
